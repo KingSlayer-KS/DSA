0a1ede5b14f73c6523c54c127433fc74ba883149
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-02-28 01:27:14 +0530

    a

--- Sorting_types/Insertion_sort.java
+++ Sorting_types/Insertion_sort.java
@@ -2,22 +2,28 @@ package Sorting_types;
 
 public class Insertion_sort {
     private static void PrintArray(int[] arr) {
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
+
     public static void main(String[] args) {
-        int[] arr={7,8,3,1,2};
-        
-        //Insertion_sort
-        
+        int[] arr = { 7, 8, 3, 1, 2 };
 
+        // Insertion_sort
+        for (int i = 1; i < arr.length; i++) {
+            // why i=1 because we concidered the first element at index[0] to be sorted.
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
 
-
-        //Time_complixety_of_Insertion_sort_is -> O(n^2)
+        // Time_complixety_of_Insertion_sort_is -> O(n^2)
         PrintArray(arr);
     }
 
-
-    
 }
