ce7cae198481b2402b41183ace8c7c9e4954b0eb
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-02-28 00:55:26 +0530

    a

---
+++ Sorting_types/Insertion_sort.java
@@ -0,0 +1,23 @@
+package Sorting_types;
+
+public class Insertion_sort {
+    private static void PrintArray(int[] arr) {
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={7,8,3,1,2};
+        
+        //Insertion_sort
+        
+
+
+
+        //Time_complixety_of_Insertion_sort_is -> O(n^2)
+        PrintArray(arr);
+    }
+
+
+    
+}
