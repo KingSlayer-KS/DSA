15b6346a275268ae3aeb8d5a9f949cf8ac23837f
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-01 19:47:35 +0530

    a

---
+++ Recursion/Print_nos_5_to_1_using_recursion.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Print_nos_5_to_1_using_recursion {
+    public static void number(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        number(--n);
+        /*
+         * if (n >= 2) {
+         * n--;
+         * number(n);
+         * }
+         */
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        number(n);
+    }
+}
