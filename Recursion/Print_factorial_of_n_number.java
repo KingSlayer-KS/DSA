8382f5ff4950fcb63b298aa491eacf9121f545be
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-01 22:31:36 +0530

    a

---
+++ Recursion/Print_factorial_of_n_number.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Print_factorial_of_n_number {
+    public static void factorial(int n,int fact){
+        if(n  == 1){
+            System.out.println(fact);
+            return;
+        }
+         fact=fact*n;
+        factorial(n-1,fact);
+
+
+    }
+    public static void main(String[] args) {
+        int n=5;
+        factorial(n,1);
+
+
+    }
+    
+}
