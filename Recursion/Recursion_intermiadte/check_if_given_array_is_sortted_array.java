e5efc176405c19851d5147b792be05d722ab2162
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-09 01:33:11 +0530

    a

--- Recursion/Recursion_intermiadte/check_if_given_array_is_sortted_array.java
+++ Recursion/Recursion_intermiadte/check_if_given_array_is_sortted_array.java
@@ -1,8 +1,21 @@
 package Recursion.Recursion_intermiadte;
 
 public class check_if_given_array_is_sortted_array {
-    public static void main(String[] args) {
-        
+    public static void sorts(int[] aa, int i) {
+        if (i == aa.length) {
+            System.out.println("Yes");
+            return;
+        }
+        if(aa[i-1]>aa[i] ){
+            System.out.println("NO");
+            return;
+        }
+        sorts(aa, i+1);
     }
-    
+
+    public static void main(String[] args) {
+        int[] aa = { 1, 2, 4, 5, 5,5, 6, 7 };
+        sorts(aa, 1);
+    }
+
 }
