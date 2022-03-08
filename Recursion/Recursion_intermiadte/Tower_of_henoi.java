071b92c5eaab4d900fef8b9e71421ee281c2318e
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-08 22:47:28 +0530

    a

---
+++ Recursion/Recursion_intermiadte/Tower_of_henoi.java
@@ -0,0 +1,20 @@
+package Recursion.Recursion_intermiadte;
+//complexity= 2^n
+public class Tower_of_henoi {
+    public static void tower(int n,String src,String help, String dst){
+        if(n==1){
+            System.out.println("transfer "+ n +" disk"+" from "+src+" to "+dst);
+            return;
+        }
+        tower(n-1,src,dst,help);
+        System.out.println("transfer "+ n +" disk"+" from "+src+" to "+dst);
+        tower(n-1,help,src,dst);
+        
+    }
+    public static void main(String[] args) {
+        int n=3;
+        tower(n,"S","H","D");
+
+    }
+    
+}
