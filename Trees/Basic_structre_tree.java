ea5e4ae5f227e11f0a2baf1b8a921752c170602a
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-04-12 14:00:00 +0530

    .

--- Trees/Basic_structre_tree.java
+++ Trees/Basic_structre_tree.java
@@ -1,5 +1,42 @@
 package Trees;
 
-public  class Basic_structre_tree {
+public class Basic_structre_tree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;// for traversal
+
+        public Node buildtree(int[] nodes) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node new_node = new Node(nodes[index]);
+            new_node.left = buildtree(nodes);
+            new_node.right = buildtree(nodes);
+
+            return new_node;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildtree(nodes);
+        System.out.println(root.data);
+    }
 
 }
